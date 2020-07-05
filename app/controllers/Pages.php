8f05c5ba0dde1dc214687b67cfb2c0956222e896
hajaraitel <hajaraitel@gmail.com>
2020-07-05 20:18:48 +0100

    added methods to the database class to handle data

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,12 +3,14 @@ Class Pages extends Controller
 {
     public function __construct()
     {
-        $this->postModel=$this->model('Post');
+        //$this->postModel=$this->model('Post');
     }
     
     public function index()
     {
+        //$users=$this->postModel->getUsers();
         $data=['title'=>'Welcome'];
+        
         $this->view('pages/index',$data);
     }
 
--- app/libraries/database.php
+++ app/libraries/database.php
@@ -35,4 +35,57 @@ class Database {
         }
     }
 
+    public function query($sql)
+    {
+        $this->stmt=$this->dbh->prepare($sql);
+    }
+
+    //bind values to parameters
+    public function bind($value,$param,$type=null)
+    {
+        if(is_null($type))
+        {
+            switch(true)
+            {
+                case is_int($type):
+                    $type=PDO::PARAM_INT;
+                    break;
+                case is_bool($type):
+                    $type=PDO::PARAM_BOOL;
+                    break;
+                case is_null($type):
+                    $type=PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type=PDO::PARAM_STR;
+
+            }
+        }
+        $this->stmt->bindValue($param,$value,$type);
+    }
+
+    //execute the query
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
+
+    //get result set as array of objects
+    public function getAll()
+    {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    //get single object
+    public function getObject()
+    {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function RowCount()
+    {
+        return $this->stmt->rowCount();
+    }
 }
